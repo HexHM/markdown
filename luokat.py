7b4f69ce072bde2f649b13b48fa1d5213b1502d7
HexHM <53179@edu.raseko.fi>
2025-01-08 12:50:49 +0200

    luokka harjoituksia

--- luokat.py
+++ luokat.py
@@ -12,3 +12,22 @@ henkil1 = henkil()
 henkil1.sukunim = "turtiainen"
 
 print("Tämän henkilön etunimi on", henkil1.etunim, "ja sukunimi on", henkil1.sukunim)
+
+print("---------------------------------")
+
+# luokka, johon on rakennettu muodostin ja metodi
+
+class koodari:
+    # Oliomuodostin, argumentteina etu- ja sukunimi, self viittaa luokasta luotavaan olioon
+    def __init__(self, etunimi, sukunimi):
+        self.etunimi = etunimi
+        self.sukunimi = sukunimi
+    
+    # Metodi joka tulostaa ruudulle viestin
+    def oma_kehu(self):
+        print("mä oon", self.etunimi, "ja onn paras koodari ikinä")
+# luodaan koodari1-olio muodostimella
+koodari1 = koodari ("Jonne", "Janttari")
+
+# Kutsutaan koodari1:n oma_kehu-meodia
+koodari1.oma_kehu()
